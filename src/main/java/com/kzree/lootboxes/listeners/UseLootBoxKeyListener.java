4ab1d4315e8102bd68ffba06dbbe72a3c6eaf330
Erkki Sula <43129445+kzree@users.noreply.github.com>
2022-05-02 01:15:35 +0300

    Create lootbox gui, right click only event

--- src/main/java/com/kzree/lootboxes/listeners/UseLootBoxKeyListener.java
+++ src/main/java/com/kzree/lootboxes/listeners/UseLootBoxKeyListener.java
@@ -1,12 +1,16 @@
 package com.kzree.lootboxes.listeners;
 
+import com.kzree.lootboxes.LootBoxRarity;
 import com.kzree.lootboxes.LootBoxes;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
@@ -18,8 +22,15 @@ public class UseLootBoxKeyListener implements Listener {
         this.plugin = plugin;
     }
 
-    private void handleLootBoxUse(Player player, ItemStack lootboxKey) {
+    // TODO: Improve lootbox type displaying
+    private void createLootBoxGUI(Player player, LootBoxRarity lootBoxRarity) {
+        Inventory gui = Bukkit.createInventory(player, 27, "Loot box ("+ lootBoxRarity.name().toLowerCase() + ")");
+        player.openInventory(gui);
+    }
+
+    private void handleLootBoxUse(Player player, ItemStack lootboxKey, LootBoxRarity lootBoxRarity) {
         player.sendMessage(ChatColor.GREEN + "You just tried to open a lootbox!");
+        createLootBoxGUI(player, lootBoxRarity);
 
         if (lootboxKey.getAmount() > 1) {
             lootboxKey.setAmount(lootboxKey.getAmount() - 1);
@@ -28,16 +39,31 @@ public class UseLootBoxKeyListener implements Listener {
         }
     }
 
+    // TODO: Maybe put this in an utils file
+    private LootBoxRarity getLootboxRarityFromString(String rarityString) {
+        LootBoxRarity rarity;
+        try {
+            rarity = LootBoxRarity.valueOf(rarityString);
+        } catch (IllegalArgumentException e) {
+            rarity = LootBoxRarity.COMMON;
+        }
+        return rarity;
+    }
+
     @EventHandler
     public void onLootBoxKeyUse(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        Action action = event.getAction();
         ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
         ItemMeta itemInMainHandItemMeta = itemInMainHand.getItemMeta();
-        NamespacedKey namespacedKey = new NamespacedKey(this.plugin, "item-type");
-        if (itemInMainHandItemMeta != null) {
-            String keyValue = itemInMainHandItemMeta.getPersistentDataContainer().get(namespacedKey, PersistentDataType.STRING);
-            if (keyValue != null && keyValue.equals("loot-key")) {
-                handleLootBoxUse(player, itemInMainHand);
+        if (itemInMainHandItemMeta != null && (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)) {
+            String itemTypeKeyValue = itemInMainHandItemMeta.getPersistentDataContainer()
+                    .get(new NamespacedKey(this.plugin, "item-type"), PersistentDataType.STRING);
+            String rarityKeyValue = itemInMainHandItemMeta.getPersistentDataContainer()
+                    .get(new NamespacedKey(this.plugin, "rarity"), PersistentDataType.STRING);
+            if (itemTypeKeyValue != null && rarityKeyValue != null && itemTypeKeyValue.equals("loot-key")) {
+                LootBoxRarity rarity = getLootboxRarityFromString(rarityKeyValue);
+                handleLootBoxUse(player, itemInMainHand, rarity);
             }
         }
     }
